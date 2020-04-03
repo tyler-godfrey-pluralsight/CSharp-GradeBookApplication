b62f88e13aa05d914c0d1f6a248827a6dbcc1813
Tyler Godfrey <tyler.r.godfrey@emcins.com>
2020-04-03 07:48:00 -0500

    Added GradeBookTypes, RankedGradeBook, and StandardGradeBook

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,6 +11,7 @@ namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
+        public GradeBookType Type;
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
